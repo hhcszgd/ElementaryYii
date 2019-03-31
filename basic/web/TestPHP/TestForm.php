b98a091872a42a4a82e94cead46b0565cc193de4
WY <wy@WYdeMac-mini.local>
2019-03-31 16:31:26 +0800

    test html

---
+++ basic/web/TestPHP/TestForm.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wy
+ * Date: 2019/3/31
+ * Time: 16:14
+ */
+
+
+$username = $_POST["username"];
+$passcode= $_POST["passcode"];
+echo "你输入的用户名是:" . $username;
+echo "你输入的密码是:" . $passcode;

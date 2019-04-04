de6dcc6673e31ac59e0bfb6b69d78aea9c9bcfeb
WY <wy@WYdeMac-mini.local>
2019-04-04 15:03:03 +0800

    add check

--- basic/web/wechat/checkSignature.php
+++ basic/web/wechat/checkSignature.php
@@ -11,16 +11,16 @@ private function checkSignature()
     $signature = _GET["signature"];
     $timestamp = _GET["timestamp"];
     $nonce  = _GET["nonce"];
-
-    $tmpArr = array($timestamp, $nonce);
+    $token = "hhcszgd";
+    $tmpArr = array($token , $timestamp, $nonce);
     sort($tmpArr, SORT_STRING);
     $tmpStr = implode( $tmpArr );
     $tmpStr = sha1( $tmpStr );
 
-    if( $signature ){
-        return true;
+    if( $signature == $tmpStr){
+        return _GET["echostr"];;
     }else{
-        return false;
+        return "";
     }
 }
 checkSignature();

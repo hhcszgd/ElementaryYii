03cc785893d838ad61f5e25b25c2578a632171f6
WY <wy@WYdeMac-mini.local>
2019-04-04 16:39:04 +0800

    test

--- basic/web/wechat/checkSignature.php
+++ basic/web/wechat/checkSignature.php
@@ -8,21 +8,22 @@
     $token="hhcszgd";
     $echostr=$_GET["echostr"];
     $tmpArr=array($token , $timestamp, $nonce);
-    sort($tmpArr);
+    sort($tmpArr , 2);
     $tmpStr=implode( $tmpArr );
     $tmpStr1=sha1( $tmpStr );
-    print_r($signature);
-//    print_r(" signature:{$signature} "  );
-//    print_r(" timestamp:{$timestamp} "  );
-//    print_r( " nonce : {$nonce}"  );
-//    print_r(" token: {$token}"  );
-//    print_r(" tmpArr : {$tmpArr}"  );
-//    print_r(" tmpStr : {$tmpStr}" );
-//    print_r(" tmpStr1 : {$tmpStr1}" );
+
+    print_r(" signature:{$signature} "  );
+    print_r(" timestamp:{$timestamp} "  );
+    print_r( " nonce : {$nonce}"  );
+    print_r(" token: {$token}"  );
+    print_r(" tmpArr : {$tmpArr}"  );
+    print_r(" tmpStr : {$tmpStr}" );
+    print_r(" tmpStr1 : {$tmpStr1}" );
+    echo "xxxxxx";
     if( $signature == $tmpStr1){
-        return $echostr;
+        echo $echostr;
     }else{
-        return "";
+        echo "出错了";
     }
 
 

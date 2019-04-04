9002dcfe26594f00aa6fcb03b102d7cdc3f62439
WY <wy@WYdeMac-mini.local>
2019-04-04 15:57:39 +0800

    test

--- basic/web/wechat/checkSignature.php
+++ basic/web/wechat/checkSignature.php
@@ -11,7 +11,13 @@
     sort($tmpArr);
     $tmpStr = implode( $tmpArr );
     $tmpStr1 = sha1( $tmpStr );
-    print_r($tmpStr1);
+    print_r(" signature: " . $signature);
+    print_r(" timestamp: " . $timestamp);
+    print_r( " nonce : " . $nonce);
+    print_r(" token:" . $token);
+    print_r(" tmpArr : " . $tmpArr);
+    print_r(" tmpStr : " . $tmpStr);
+    print_r(" tmpStr1 : " . $tmpStr1);
     if( $signature == $tmpStr1){
         return _GET["echostr"];
     }else{
@@ -21,3 +27,6 @@
 checkSignature();
 
 //http://101.200.45.131/wechat/checkSignature.php?signature=4fb62c5f431613dd83f5da66c849ca9e6b90aece&echostr=7019144854469720927&timestamp=1554362711&nonce=522026408
+
+ccbc11b090e30b7ae07f9c8e7411ce14b6489d76
+9debd7ae5180146185962bf9141bcf083c7c3cc4

0f949dc2ddf147604df259d893e5475d9ab6abc4
WY <wy@WYdeMac-mini.local>
2019-04-04 21:03:06 +0800

    test

--- basic/web/wechat/checkSignature.php
+++ basic/web/wechat/checkSignature.php
@@ -1,14 +1,16 @@
 <?php
-header('Content-type: text/plain');
-$signature=$_GET["signature"];
-$timestamp=$_GET["timestamp"];
-$nonce=$_GET["nonce"];
-$token="hhcszgd";
-$echostr=$_GET['echostr'];
-$tmpArr=array($token , $timestamp, $nonce);
-sort($tmpArr , 2);
-$tmpStr=implode( $tmpArr );
-$tmpStr1=sha1( $tmpStr );
+
+function checkoutToken(){
+    header('Content-type: text/plain');
+    $signature=$_GET["signature"];
+    $timestamp=$_GET["timestamp"];
+    $nonce=$_GET["nonce"];
+    $token="hhcszgd";
+    $echostr=$_GET['echostr'];
+    $tmpArr=array($token , $timestamp, $nonce);
+    sort($tmpArr , 2);
+    $tmpStr=implode( $tmpArr );
+    $tmpStr1=sha1( $tmpStr );
 //print_r(" echostr:{$echostr} "  );
 //print_r(" signature:{$signature} "  );
 //print_r(" timestamp:{$timestamp} "  );
@@ -18,12 +20,14 @@ $tmpStr1=sha1( $tmpStr );
 //print_r(" tmpStr : {$tmpStr}" );
 //print_r(" tmpStr1 : {$tmpStr1}" );
 //echo "xxxxxx";
-if( $signature==$tmpStr1){
+    if( $signature==$tmpStr1){
 //    return $echostr;
-    echo $echostr;
-}else{
-    return "ss";
+        echo $echostr;
+    }else{
+        return "ss";
 //    echo "sssssss";
 //    return "failure";
+    }
 }
+checkoutToken();
 ?>

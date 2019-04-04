1d89054cc3d4e960f258b943150e3c86c204dc06
WY <wy@WYdeMac-mini.local>
2019-04-04 16:30:30 +0800

    test

--- basic/web/wechat/checkSignature.php
+++ basic/web/wechat/checkSignature.php
@@ -1,17 +1,17 @@
 <?php
 
 
- function checkSignature()
-{
-    $signature = _GET["signature"];
-    $timestamp = _GET["timestamp"];
-    $nonce  = _GET["nonce"];
-    $token = "hhcszgd";
-    $tmpArr = array($token , $timestamp, $nonce);
+
+    $signature=$_GET["signature"];
+    $timestamp=$_GET["timestamp"];
+    $nonce=$_GET["nonce"];
+    $token="hhcszgd";
+    $echostr=$_GET["echostr"];
+    $tmpArr=array($token , $timestamp, $nonce);
     sort($tmpArr);
-    $tmpStr = implode( $tmpArr );
-    $tmpStr1 = sha1( $tmpStr );
-    print_r($_GET);
+    $tmpStr=implode( $tmpArr );
+    $tmpStr1=sha1( $tmpStr );
+    print_r($signature);
 //    print_r(" signature:{$signature} "  );
 //    print_r(" timestamp:{$timestamp} "  );
 //    print_r( " nonce : {$nonce}"  );
@@ -20,11 +20,10 @@
 //    print_r(" tmpStr : {$tmpStr}" );
 //    print_r(" tmpStr1 : {$tmpStr1}" );
     if( $signature == $tmpStr1){
-        return _GET["echostr"];
+        return $echostr;
     }else{
         return "";
     }
-}
-checkSignature();
+
 
 //http://101.200.45.131/wechat/checkSignature.php?signature=4fb62c5f431613dd83f5da66c849ca9e6b90aece&echostr=7019144854469720927&timestamp=1554362711&nonce=522026408

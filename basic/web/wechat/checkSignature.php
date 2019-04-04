bcf1c4a566c359ed4f49ec9397924a9e5e62ad78
WY <wy@WYdeMac-mini.local>
2019-04-04 20:36:19 +0800

    test

--- basic/web/wechat/checkSignature.php
+++ basic/web/wechat/checkSignature.php
@@ -9,15 +9,15 @@ $tmpArr=array($token , $timestamp, $nonce);
 sort($tmpArr , 2);
 $tmpStr=implode( $tmpArr );
 $tmpStr1=sha1( $tmpStr );
-print_r(" echostr:{$echostr} "  );
-print_r(" signature:{$signature} "  );
-print_r(" timestamp:{$timestamp} "  );
-print_r( " nonce : {$nonce}"  );
-print_r(" token: {$token}"  );
-print_r(" tmpArr : {$tmpArr}"  );
-print_r(" tmpStr : {$tmpStr}" );
-print_r(" tmpStr1 : {$tmpStr1}" );
-echo "xxxxxx";
+//print_r(" echostr:{$echostr} "  );
+//print_r(" signature:{$signature} "  );
+//print_r(" timestamp:{$timestamp} "  );
+//print_r( " nonce : {$nonce}"  );
+//print_r(" token: {$token}"  );
+//print_r(" tmpArr : {$tmpArr}"  );
+//print_r(" tmpStr : {$tmpStr}" );
+//print_r(" tmpStr1 : {$tmpStr1}" );
+//echo "xxxxxx";
 if( $signature == $tmpStr1){
     return $echostr;
 }else{
